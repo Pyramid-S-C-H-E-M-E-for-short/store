5d45bd5160326c3dc209441e9cd19ee432d24753
Ben Halverson <benhalverson33@gmail.com>
2024-09-04 01:41:28 -0700

    no-cors

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -3,7 +3,9 @@ import { useEffect } from 'react'
 const BASE_URL = 'https://3dprinter-web-api.benhalverson.workers.dev/list'
 
 const getData = async () => {
-  const response = await fetch(BASE_URL)
+  const response = await fetch(BASE_URL, {
+    mode: 'no-cors',
+  })
   const data = await response.json()
   console.log('data', data)
   return data
