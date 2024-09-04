dea46e4e368484a1fafdf77482e3bc01462f6ec3
Ben Halverson <benhalverson33@gmail.com>
2024-09-04 02:13:17 -0700

    listing works

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,24 +1,41 @@
-import { useEffect } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
 
-const BASE_URL = 'https://3dprinter-web-api.benhalverson.workers.dev/list'
-
-const getData = async () => {
-  const response = await fetch(BASE_URL, {
-    mode: 'no-cors'
-  })
-  const data = await response.json()
-  console.log('data', data)
-  return data
-}
-
+const BASE_URL = "https://3dprinter-web-api.benhalverson.workers.dev/list";
 
 function ProductList() {
-useEffect(() => {
-  getData()
-}, [])
-  return (
-    <div>ProductList</div>
-  )
+	const [products, setProducts] = useState<ProductResponse[]>([]);
+	const getData = async () => {
+		const response = await fetch(BASE_URL);
+
+		try {
+			const data = (await response.json()) as ProductResponse[];
+
+			setProducts(data);
+			console.log("products", products);
+			console.log("typeof", typeof products);
+			return data;
+		} catch (error) {
+			console.error("error", error);
+		}
+	};
+
+	useEffect(() => {
+		getData();
+	}, []);
+	return (
+		<ul>
+			{products.map((product, ) => (
+				<li key={product.version}>{product.stl}</li>
+			))}
+		</ul>
+	);
 }
 
-export default ProductList
+export default ProductList;
+
+interface ProductResponse {
+  size: string;
+	stl: string;
+  version: string;
+}
