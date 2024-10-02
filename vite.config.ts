c08d8869eb962721b904f5461b8cc487f35668d7
Ben Halverson <benhalverson33@gmail.com>
2024-10-01 22:27:46 -0700

    config again

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': process.env
+    'process.env.VITE_BASE_URL': JSON.stringify(process.env.VITE_BASE_URL)
   }
 })
