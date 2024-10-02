e193d17edbdbe5ffb42d441391818e99950930f9
Ben Halverson <benhalverson33@gmail.com>
2024-10-01 22:45:46 -0700

    revert

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env.VITE_BASE_URL': JSON.stringify(process.env.VITE_BASE_URL)
-  }
+  // define: {
+  //   'process.env.VITE_BASE_URL': JSON.stringify(process.env.VITE_BASE_URL)
+  // }
 })
