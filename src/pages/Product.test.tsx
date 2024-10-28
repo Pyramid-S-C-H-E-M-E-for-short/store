7895689119684e719ccfe4ccbbc6e463089eb6a4
Ben Halverson <benhalverson33@gmail.com>
2024-10-28 02:06:20 -0700

    fixed linting errors

--- src/pages/Product.test.tsx
+++ src/pages/Product.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen, fireEvent, waitFor, act } from "@testing-library/react";
 import ProductPage from "./Product";
 import { vi } from "vitest";
-import React, { Suspense } from "react";
+import { Suspense } from "react";
 
 // Mock lazy-loaded component
 vi.mock("../components/PreviewComponent", () => ({
